e5170dc1d239f573e52c5e56b5da61d764ef39bc
fangweiqiang <weiqiang@adbright.cn>
2020-07-17 11:21:25 +0800

    add sql build insert

--- mysql.go
+++ mysql.go
@@ -3,6 +3,8 @@ package goconf
 import (
 	"database/sql"
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -63,4 +65,87 @@ func InitGorm(key string) *gorm.DB {
 	db.DB().SetMaxOpenConns(conf.OpenConn)
 	db.DB().SetConnMaxLifetime(time.Second * time.Duration(conf.LifeTime))
 	return db
+}
+
+//GetInsertSQL 获取sql
+func GetInsertSQL(table string, sets map[string]interface{}) string {
+	keys := []string{}
+	values := []string{}
+	for idx := range sets {
+		value := sets[idx]
+		switch value.(type) {
+		case string:
+			keys = append(keys, idx)
+			values = append(values, "'"+value.(string)+"'")
+		case int, int16, int32, int64, int8, uint, uint16, uint32, uint64, float32, float64:
+			keys = append(keys, idx)
+			var x string
+			switch value.(type) {
+			case int:
+				x = strconv.FormatInt(int64(value.(int)), 10)
+			case int16:
+				x = strconv.FormatInt(int64(value.(int16)), 10)
+			case int32:
+				x = strconv.FormatInt(int64(value.(int32)), 10)
+			case int64:
+				x = strconv.FormatInt(int64(value.(int64)), 10)
+			case int8:
+				x = strconv.FormatInt(int64(value.(int8)), 10)
+			case uint:
+				x = strconv.FormatUint(uint64(value.(uint)), 10)
+			case uint16:
+				x = strconv.FormatUint(uint64(value.(uint16)), 10)
+			case uint32:
+				x = strconv.FormatUint(uint64(value.(uint32)), 10)
+			case uint64:
+				x = strconv.FormatUint(uint64(value.(uint64)), 10)
+			case float32:
+				x = strconv.FormatFloat(float64(value.(float32)), 'f', -6, 32)
+			case float64:
+				x = strconv.FormatFloat(float64(value.(float64)), 'f', -6, 64)
+			}
+			values = append(values, x)
+		default:
+			panic("GetInsertSQL type error")
+		}
+	}
+	fmt.Println(keys)
+	fmt.Println(values)
+	sql := fmt.Sprintf("insert into %v (%v) values (%v)", table, strings.Join(keys, ","), strings.Join(values, ","))
+	return sql
+}
+
+//ExecuteSQL sql转换
+func ExecuteSQL(table string, sets map[string]interface{}, db *sql.DB) (int64, error) {
+	keys := []string{}
+	values := []string{}
+	objs := []interface{}{}
+	for idx := range sets {
+		value := sets[idx]
+		switch value.(type) {
+		case string:
+			keys = append(keys, idx)
+			values = append(values, "?")
+		case int, int16, int32, int64, int8, uint, uint16, uint32, uint64, float32, float64:
+			keys = append(keys, idx)
+			values = append(values, "?")
+		default:
+			panic("GetInsertSQL type error")
+		}
+		objs = append(objs, value)
+	}
+	sqlx := fmt.Sprintf("insert into %v (%v) values (%v)", table, strings.Join(keys, ","), strings.Join(values, ","))
+
+	stmt, err := db.Prepare(sqlx)
+	if err != nil {
+		fmt.Println(err, "Prepare")
+		return 0, err
+	}
+	result, err := stmt.Exec(objs...)
+	if err != nil {
+		fmt.Println(err, "Exec")
+		return 0, err
+	}
+	return result.LastInsertId()
+
 }
---
+++ mysql_test.go
@@ -0,0 +1,23 @@
+package goconf
+
+import (
+	"testing"
+)
+
+func Test_Mysql(t *testing.T) {
+	// GetInsertSQL("xxx", map[string]interface{}{
+	// 	"1": 1,
+	// 	"2": "2",
+	// 	"3": "3",
+	// 	"4": "xx",
+	// 	"5": 1.23,
+	// })
+
+	// ExecuteSQL("xxxx", map[string]interface{}{
+	// 	"1": 1,
+	// 	"2": "2",
+	// 	"3": "3",
+	// 	"4": "xx",
+	// 	"5": 1.23,
+	// },)
+}
