9568806c4cd8d1929dc5fc59b46cb7f78e3f14d1
fangweiqiang <weiqiang@adbright.cn>
2020-08-11 10:27:43 +0800

    update

--- csv.go
+++ csv.go
@@ -9,7 +9,7 @@ import (
 )
 
 //ReadCsv csv 文件读取
-func ReadCsv(path string, message chan []string, f func([]string, int), fNum int) error {
+func ReadCsv(path string, message chan []string, midf func([]string, int), end func(int), fNum int) error {
 	file, err := os.Open(path)
 	defer file.Close()
 	if err != nil {
@@ -27,24 +27,25 @@ func ReadCsv(path string, message chan []string, f func([]string, int), fNum int
 		}
 		count++
 		if count%fNum == 0 {
-			f(recoder, count)
+			midf(recoder, count)
 		}
 		message <- recoder
 	}
+	end(count)
 	return nil
 }
 
 //ReadCsvWorker 读取文件,并且有worker 消耗文件
-//path 文件路径 f 读取函数中间操作, fNum读取多少个文件执行f函数
+//path 文件路径 midf 读取函数中间操作, fNum读取多少个文件执行f函数
 //worker 工作池 ,workerNumber  工作池数量
-func ReadCsvWorker(path string, f func([]string, int), fNum int, worker func(chan []string, *sync.WaitGroup), workerNumber int) error {
+func ReadCsvWorker(path string, midf func([]string, int), endf func(int), fNum int, worker func(chan []string, *sync.WaitGroup), workerNumber int) error {
 	message := make(chan []string, 10000)
 	var wg sync.WaitGroup
 	for i := 0; i < workerNumber; i++ {
 		wg.Add(1)
 		go worker(message, &wg)
 	}
-	err := ReadCsv(path, message, f, fNum)
+	err := ReadCsv(path, message, midf, endf, fNum)
 	for i := 0; i < workerNumber; i++ {
 		message <- []string{"ok"}
 	}
